213e858caa037e7ac26d429d951e4d8def43c772
Shockshwat <shashwatgautam696@gmail.com>
2022-09-19 15:46:11 +0530

    added functionality

--- public/Js/main.js
+++ public/Js/main.js
@@ -5,22 +5,41 @@ cad2.addEventListener("click", active2);
 document.getElementById("countData").addEventListener("click", senddata);
 
 let selected = null;
+
 function active1() {
-	if (cad1.style.backgroundColor === "green") {
-	} else {
-		cad1.style.backgroundColor = "green";
-		cad2.style.backgroundColor = "#8a8a8a38";
-		selected = cad1.value;
-	}
+    if (cad1.style.backgroundColor != "green") {
+        cad1.style.backgroundColor = "green";
+        cad2.style.backgroundColor = "#8a8a8a38";
+        selected = cad1.value;
+    }
 }
+
 function active2() {
-	if (cad2.style.backgroundColor === "green") {
-	} else {
-		cad2.style.backgroundColor = "green";
-		cad1.style.backgroundColor = "#8a8a8a38";
-		selected = cad2.value;
-	}
+    if (cad2.style.backgroundColor != "green") {
+        cad2.style.backgroundColor = "green";
+        cad1.style.backgroundColor = "#8a8a8a38";
+        selected = cad2.value;
+    }
 }
+
 function senddata() {
-	console.log(selected);
+    if (selected == null) {
+        document.getElementById("error_message").innerText =
+            "Please click on one candidate";
+    } else {
+        document.querySelector("#error_message").innerText = "";
+        console.log(selected);
+        update_data(selected);
+    }
+}
+
+function update_data(data) {
+    //file.write(data) (in Documents folder) please put respective JS here
+    // DATA will be the JSON.
+    // cad1.value = data[i[0]];
+    // cad2.value = data[i[1]];
+    cad1.style.backgroundColor = "#8a8a8a38";
+    cad2.style.backgroundColor = "#8a8a8a38";
+    cad1.value = "your";
+    cad2.value = "mother";
 }
